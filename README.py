df19d52a349cf1a7c26c5f95167d3d82196bf0c1
mhmdreza <165557282+MRNR200@users.noreply.github.com>
2024-07-10 23:14:43 +0330

    Update README.py

--- README.py
+++ README.py
@@ -1,4 +1,5 @@
 # TeamProject
+#backend is :
 import random 
 javab = random.randint(1,99)
 name = input('what is your name?  ')
@@ -19,3 +20,37 @@ while hads != javab:
         exit()
     
 print('Wooooooooooow', name ,'you did it :))))')
+
+#frontend is :
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Computer" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test js</title>
+    
+    <!-- <script>
+     alert("hello golpesar")
+     
+    </script> -->
+<style>
+*{
+    background-color: rgb(88, 169, 142);
+}
+</style>
+</head>
+<body>
+    <h2><center>Test JAVA project </center></h2>
+</body>
+</html>
+
+#in develop marge
+
+# Devops has :
+#!/bin/bash
+echo "plz tell me name:"
+read name
+
+echo " hi $name! welcome."
